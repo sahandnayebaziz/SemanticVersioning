4f9d4a149afa234439659e8f12badfd2663fca34
Sahand Nayebaziz <sahand@sahand.me>
2020-08-25 11:42:43 -0700

    +init

--- Sources/SemanticVersioning/SemanticVersioning.swift
+++ Sources/SemanticVersioning/SemanticVersioning.swift
@@ -1,3 +1,75 @@
-struct SemanticVersioning {
-    var text = "Hello, World!"
+import Foundation
+
+public struct SemanticVersion: Comparable {
+    public var string: String
+    
+    public init(string: String) {
+        self.string = string
+    }
+    
+    public var components: SemanticVersionComponents {
+        makeComponents()
+    }
+    
+    public static func < (lhs: SemanticVersion, rhs: SemanticVersion) -> Bool {
+        lhs.components < rhs.components
+    }
+    
+    public func isFullyCompatible(with other: SemanticVersion) -> Bool {
+        self >= other
+    }
+    
+    private func makeComponents() -> SemanticVersionComponents {
+        var major: Int = 0
+        var minor: Int = 0
+        var patch: Int = 0
+        
+        let stringComponents = string
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .components(separatedBy: ".")
+        
+        if stringComponents.indices.contains(2) {
+            let patchComponent = stringComponents[2]
+            if let number = makeInteger(patchComponent) {
+                patch = number
+            }
+        }
+        
+        if stringComponents.indices.contains(1) {
+            let minorComponent = stringComponents[1]
+            if let number = makeInteger(minorComponent) {
+                minor = number
+            }
+        }
+        
+        if stringComponents.indices.contains(0) {
+            let majorComponent = stringComponents[0]
+            if let number = makeInteger(majorComponent) {
+                major = number
+            }
+        }
+        
+        return .init(major: major, minor: minor, patch: patch)
+    }
+    
+    private func makeInteger(_ string: String) -> Int? {
+        guard let number = NumberFormatter().number(from: string) else { return nil }
+        return Int(truncating: number)
+    }
+}
+
+public struct SemanticVersionComponents: Equatable, Comparable {
+    var major: Int
+    var minor: Int
+    var patch: Int
+    
+    public static func < (lhs: SemanticVersionComponents, rhs: SemanticVersionComponents) -> Bool {
+        if lhs.major != rhs.major {
+            return lhs.major < rhs.major
+        } else if lhs.minor != rhs.minor {
+            return lhs.minor < rhs.minor
+        } else {
+            return lhs.patch < rhs.patch
+        }
+    }
 }
--- Tests/SemanticVersioningTests/SemanticVersioningTests.swift
+++ Tests/SemanticVersioningTests/SemanticVersioningTests.swift
@@ -2,14 +2,45 @@ import XCTest
 @testable import SemanticVersioning
 
 final class SemanticVersioningTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SemanticVersioning().text, "Hello, World!")
+    
+    func testCreation() {
+        let expected1 = SemanticVersionComponents(major: 1, minor: 0, patch: 0)
+        let actual1 = SemanticVersion(string: "1.0.0").components
+        XCTAssertEqual(expected1, actual1)
+        
+        let expected123 = SemanticVersionComponents(major: 1, minor: 2, patch: 3)
+        let actual123 = SemanticVersion(string: "1.2.3").components
+        XCTAssertEqual(expected123, actual123)
+        
+        let expectedShort = SemanticVersionComponents(major: 9, minor: 0, patch: 0)
+        let actualShort = SemanticVersion(string: "9").components
+        XCTAssertEqual(expectedShort, actualShort)
+        
+        let expectedMedium = SemanticVersionComponents(major: 9, minor: 12, patch: 0)
+        let actualMedium = SemanticVersion(string: "9.12").components
+        XCTAssertEqual(expectedMedium, actualMedium)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    func testCompatability() {
+        let expectedSame = true
+        let actualSame = SemanticVersion(string: "1.0.8").isFullyCompatible(with: SemanticVersion(string: "1.0.8"))
+        XCTAssertEqual(expectedSame, actualSame)
+        
+        let expectedNewerPatch = false
+        let actualNewerPatch = SemanticVersion(string: "1.0.1").isFullyCompatible(with: SemanticVersion(string: "1.0.2"))
+        XCTAssertEqual(expectedNewerPatch, actualNewerPatch)
+        
+        let expectedNewerMinor = false
+        let actualNewerMinor = SemanticVersion(string: "1.0.1").isFullyCompatible(with: SemanticVersion(string: "1.1"))
+        XCTAssertEqual(expectedNewerMinor, actualNewerMinor)
+        
+        let expectedNewerMajor = false
+        let actualNewerMajor = SemanticVersion(string: "1.0.1").isFullyCompatible(with: SemanticVersion(string: "2.0"))
+        XCTAssertEqual(expectedNewerMajor, actualNewerMajor)
+        
+        let expectedOlderUgly = true
+        let actualOlderUgly = SemanticVersion(string: "4.11.23").isFullyCompatible(with: SemanticVersion(string: "3.876.2344"))
+        XCTAssertEqual(expectedOlderUgly, actualOlderUgly)
+    }
+    
 }
